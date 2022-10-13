464718070d7b07f44a745ccd51dfa9a45629c067
aobaidi88 <68615197+aobaidi88@users.noreply.github.com>
2022-10-13 17:38:10 -0600

    Upload (#1)
    Uploaded Refresh and JSON scripts

---
+++ DesktopRefresh.ps1
@@ -0,0 +1,15 @@
+﻿$shell = New-Object -ComObject Shell.Application
+
+$shell.minimizeall()
+
+start-sleep 1
+
+$wsh = New-Object -ComObject Wscript.Shell
+
+$wsh.sendkeys('{F5}')
+
+start-sleep 1
+
+$shell.undominimizeall()
+
+exit
---
+++ JsonFormater.ps1
@@ -0,0 +1,29 @@
+﻿$json = Read-Host "Paste your json string"
+
+$fileName = "format"
+
+$path = "C:\"
+
+$ext = ".json"
+
+$date = Get-Date -Format "yyyyMMddTHHmmss"
+
+$jsonFile = New-Item ($path + $fileName + $date + $ext) -Type File
+
+$json | ConvertFrom-Json | ConvertTo-Json | Out-File $jsonFile
+
+$notepadplus = "C:\Program Files\Notepad++\notepad++.exe"
+
+if ( -Not (Test-Path -Path $notepadplus -PathType Leaf)) {
+
+    Start-Process notepad $jsonFile
+
+}
+
+else {
+
+    Start-Process notepad++ $jsonFile
+
+}
+
+Invoke-Item $path
